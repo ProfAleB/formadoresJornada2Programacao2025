4b02f76d3acc06f78d7a6a0a887b60f5899c4fe8
Alexandre Boeing <140010324+ProfAleB@users.noreply.github.com>
2025-06-24 11:21:03 -0300

    Aula6 att2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function inserirNome(){
     let nomeUsuario = prompt("Qual é o seu nome?");
-    let elemento = document.querySelector("nome-usuário");
+    let elemento = document.querySelector("#nome-usuário");
     elemento.textContent = nomeUsuario;
 }
 
